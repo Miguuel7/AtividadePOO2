c3d21b6f5ec0b353d2f9534966dc642ce471c368
Miguel <04754825063@senacrs.edu.br>
2026-09-18 12:25:09 -0300

    Update aluno.php

--- class/aluno.php
+++ class/aluno.php
@@ -6,6 +6,7 @@ class Aluno extends Pessoa {
     public $matricula;
     public $serie;
     public $turmaAtual;
+    public $novaTurma;
     public $notas;
     public $mediaFinal;
     public $situacao;
@@ -29,6 +30,7 @@ class Aluno extends Pessoa {
         }
     }
 
+
     public function atualizarSituacao() {
         if 
             ($this->mediaFinal>= 7) {
@@ -38,27 +40,32 @@ class Aluno extends Pessoa {
             }
     }
     
-    public function mudarTurma() {
-        
+    public function mudarTurma($novaTurma) {
+        $this->novaTurma = $novaTurma;
     }
+   
     
-    }
+}
 
 $objAluno = new aluno();
 $objAluno->notas = array(8, 7, 9);
 $objAluno->dataMatricula = "2023-01-15";
-$objAluno->serie = "null";
+$objAluno->serie = "2º Ano";
 $objAluno->turmaAtual = "201";
 $objAluno->responsavel = "Sr. Carlos da Silva";
 $objAluno->matricula = "2023001";
 $objAluno->situacao = null;
+$objAluno->novaTurma = 202;
 
+
+$objAluno->mudarTurma();
 $objAluno->notaFinal();
 $objAluno->atualizarSituacao();
 
 echo "Matrícula: " . $objAluno->matricula . "\n";
 echo "Data de Matrícula: " . $objAluno->dataMatricula . "\n";
 echo "Série: " . $objAluno->serie . "\n";
+echo "Turma" . $objAluno->novaTurma . "\n";
 echo "Turma Atual: " . $objAluno->turmaAtual . "\n";
 echo "Notas: " .  " $objAluno->mediaFinal" . "\n";
 echo "Situação: " . "$objAluno->situacao" . "\n";
